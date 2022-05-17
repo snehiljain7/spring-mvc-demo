43822a3b606f8a408738a943bd193a1d9118dcc0
Snehil Jain <snehiljain7@gmail.com>
2022-05-17 13:10:56 +0530

    Request Params and Request Mappings

--- src/com/learning/springdemo/mvc/HelloWorldController.java
+++ src/com/learning/springdemo/mvc/HelloWorldController.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	//need a controller method to show initial form
@@ -36,6 +38,22 @@ public class HelloWorldController {
 			
 			//create the message
 			String result = "Yo! "+theName;
+			
+			//add message to the model
+			model.addAttribute("message", result);
+			
+			return "helloworld";
+		}
+		
+		
+		@RequestMapping("/processFormVersionThree")
+		public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+			
+			//convert the data to all caps
+			theName = theName.toUpperCase();
+			
+			//create the message
+			String result = "Hey My Friend from v3! "+theName;
 			
 			//add message to the model
 			model.addAttribute("message", result);
